bd129236d08ee947e037bd1d382fc3fe3e40ea5e
Michael Etim <michaelmunchin89@gmail.com>
2020-09-29 16:24:51 +0100

    Added Blazor route parameters

--- Blazor/EmployeeManagement.Web/Services/EmployeeService.cs
+++ Blazor/EmployeeManagement.Web/Services/EmployeeService.cs
@@ -15,6 +15,11 @@ namespace EmployeeManagement.Web.Services
             _httpClient = httpClient;
         }
 
+        public async Task<Employee> GetEmployee( int id )
+        {
+            return await _httpClient.GetJsonAsync<Employee>( $"api/employees/{id}" );
+        }
+
         public async Task<IEnumerable<Employee>> GetEmployees( )
         {
             return await _httpClient.GetJsonAsync<Employee[]>( "api/employees" );
