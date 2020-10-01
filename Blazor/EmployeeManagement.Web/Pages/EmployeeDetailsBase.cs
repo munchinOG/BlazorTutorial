076679bc2783bd0c9f64d91d2d364dca3f75e3bf
Michael Etim <michaelmunchin89@gmail.com>
2020-10-01 12:11:52 +0100

    Blazor Event Handing

--- Blazor/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ Blazor/EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -9,6 +9,11 @@ namespace EmployeeManagement.Web.Pages
     {
         public Employee Employee { get; set; } = new Employee();
 
+        protected string Coordinates { get; set; }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
@@ -20,5 +25,24 @@ namespace EmployeeManagement.Web.Pages
             Id = Id ?? "102";
             Employee = await EmployeeService.GetEmployee( int.Parse( Id ) );
         }
+
+        protected void Button_Click( )
+        {
+            if(ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
+
+        //protected void Mouse_Move( MouseEventArgs e )
+        //{
+        //    Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        //}
     }
 }
