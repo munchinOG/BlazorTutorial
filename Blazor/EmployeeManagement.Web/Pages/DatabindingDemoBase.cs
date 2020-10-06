9f88c99fa2fb709a167c35c03eb145674f8a2276
Michael Etim <michaelmunchin89@gmail.com>
2020-10-06 10:50:57 +0100

    Blazor two way data binding

---
+++ Blazor/EmployeeManagement.Web/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DatabindingDemoBase : ComponentBase
+    {
+        public string Description { get; set; } = string.Empty;
+
+        //protected string Name { get; set; } = "Tom";
+        //protected string Gender { get; set; } = "Male";
+
+        //protected string Colour { get; set; } = "background-color: white";
+    }
+}
--- Blazor/EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ Blazor/EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -12,6 +12,8 @@ namespace EmployeeManagement.Web.Pages
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
+        public bool ShowFooter { get; set; } = true;
+
         public IEnumerable<Employee> Employees { get; set; }
 
         protected override async Task OnInitializedAsync( )
